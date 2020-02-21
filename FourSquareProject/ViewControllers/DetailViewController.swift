de49d7f5f78ea2e7421416aed832a28d472ff2cd
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-21 16:57:53 -0500

    created files and folders

---
+++ FourSquareProject/ViewControllers/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  FourSquareProject
+//
+//  Created by Amy Alsaydi on 2/21/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FourSquareProject/Views/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  FourSquareProject
+//
+//  Created by Amy Alsaydi on 2/21/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
