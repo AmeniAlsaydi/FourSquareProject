9b6d5ac818b374a2062358a9856ec35b0165fb2b
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-21 15:19:53 -0500

    Initial Commit

---
+++ FourSquareProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FourSquareProject
+//
+//  Created by Amy Alsaydi on 2/21/20.
+//  Copyright © 2020 Amy Alsaydi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
