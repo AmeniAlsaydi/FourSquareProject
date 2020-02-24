3c8f068a7a85a5cb30ab4c7e47aec869e5da4b3d
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-24 15:00:32 -0500

    edited the core locations session class

--- FourSquareProject/CoreLocation/CoreLocationSession.swift
+++ FourSquareProject/CoreLocation/CoreLocationSession.swift
@@ -9,22 +9,6 @@
 import Foundation
 import CoreLocation
 
-// core location is the back bone of
-struct Location {
-  let title: String
-  let body: String
-  let coordinate: CLLocationCoordinate2D
-  let imageName: String
-  
-  static func getLocations() -> [Location] {
-    return [
-      Location(title: "Pursuit", body: "We train adults with the most need and potential to get hired in tech, advance in their careers, and become the next generation of leaders in tech.", coordinate: CLLocationCoordinate2D(latitude: 40.74296, longitude: -73.94411), imageName: "team-6-3"),
-      Location(title: "Brooklyn Museum", body: "The Brooklyn Museum is an art museum located in the New York City borough of Brooklyn. At 560,000 square feet (52,000 m2), the museum is New York City's third largest in physical size and holds an art collection with roughly 1.5 million works", coordinate: CLLocationCoordinate2D(latitude: 40.6712062, longitude: -73.9658193), imageName: "brooklyn-museum"),
-      Location(title: "Central Park", body: "Central Park is an urban park in Manhattan, New York City, located between the Upper West Side and the Upper East Side. It is the fifth-largest park in New York City by area, covering 843 acres (3.41 km2). Central Park is the most visited urban park in the United States, with an estimated 37.5–38 million visitors annually, as well as one of the most filmed locations in the world.", coordinate: CLLocationCoordinate2D(latitude: 40.7828647, longitude: -73.9675438), imageName: "central-park")
-    ]
-  }
-}
-
 class CoreLocationSession: NSObject {
   
   public var locationManager: CLLocationManager
