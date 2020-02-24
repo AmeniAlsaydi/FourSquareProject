4e182de7a463962306397d1abe3568af7a533f40
Casandra <casandragrullon@pursuit.org>
2020-02-24 16:24:44 -0500

    added elements to MapView, added mapviewcell, added collection view datasource to mapviewcontroller

--- FourSquareProject/ViewControllers/MapViewController.swift
+++ FourSquareProject/ViewControllers/MapViewController.swift
@@ -13,16 +13,19 @@ class MapViewController: UIViewController {
     
     private var datapersistance: DataPersistence<Venue>
     
-    // intializer
+    private var mapView = MapView()
     
     init(_ dataPersistance: DataPersistence<Venue>) {
         self.datapersistance = dataPersistance
         super.init(nibName: nil, bundle: nil)
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) - error")
+    }
+    
+    override func loadView() {
+        view = mapView
     }
 
     override func viewDidLoad() {
@@ -31,10 +34,35 @@ class MapViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "Search Venue"
 
-
+        //collectionView
+        mapView.collectionView.register(MapViewCell.self, forCellWithReuseIdentifier: "mapViewCell")
+        mapView.collectionView.dataSource = self
+        mapView.collectionView.delegate = self
+        
     }
     
-
     
 
+}
+
+extension MapViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = mapView.collectionView.dequeueReusableCell(withReuseIdentifier: "mapViewCell", for: indexPath) as? MapViewCell else {
+            fatalError("could not cast to mapViewCell")
+        }
+        cell.backgroundColor = .white
+        return cell
+    }
+}
+extension MapViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let maxsize: CGSize = UIScreen.main.bounds.size
+        let itemWidth: CGFloat = maxsize.width
+        let itemHeight: CGFloat = maxsize.height * 0.20
+        return CGSize(width: itemWidth, height: itemHeight)
+    }
 }
--- FourSquareProject/Views/MapView.swift
+++ FourSquareProject/Views/MapView.swift
@@ -7,15 +7,94 @@
 //
 
 import UIKit
+import MapKit
 
 class MapView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public lazy var venueTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .line
+        textField.backgroundColor = .white
+        textField.placeholder = "search by venue"
+        return textField
+    }()
+    public lazy var locationTextField: UITextField = {
+       let textField = UITextField()
+        textField.borderStyle = .line
+        textField.backgroundColor = .white
+        textField.placeholder = "search by location"
+        return textField
+    }()
+    public lazy var mapKitView: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    public lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)
+        return cv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    private func commonInit() {
+        mapConstraints()
+        venueTextConstraints()
+        locationTextConstraints()
+        collectionViewConstraints()
+    }
+    
+    private func mapConstraints(){
+        addSubview(mapKitView)
+        mapKitView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            mapKitView.topAnchor.constraint(equalTo: topAnchor),
+            mapKitView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            mapKitView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mapKitView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 
+    private func venueTextConstraints() {
+        addSubview(venueTextField)
+        venueTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            venueTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 40),
+            venueTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            venueTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            venueTextField.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    private func locationTextConstraints() {
+        addSubview(locationTextField)
+        locationTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            locationTextField.topAnchor.constraint(equalTo: venueTextField.bottomAnchor, constant: 8),
+            locationTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            locationTextField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            locationTextField.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    private func collectionViewConstraints() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            collectionView.heightAnchor.constraint(equalTo: mapKitView.heightAnchor , multiplier: 0.2)
+        ])
+    }
+    
 }
